4ed20a2c682671488ef087fafe63237e32703b87
Damon Cook <damonacook@gmail.com>
2021-10-15 16:49:07 -0400

    ➕ ADD: Cleanup_Core_Assets class

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 
+new WPThemeScaffold\Theme\Cleanup_Core_Assets();
 new WPThemeScaffold\Theme\Theme_Setup();
 new WPThemeScaffold\Theme\Asset_Pipeline();
 
